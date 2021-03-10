fe775af8e74b4a912730103dff01d1a80675da4b
nejira-k <kadric.nejira@gmail.com>
2021-03-10 22:34:57 +0100

    Added missing files

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+
+    // This is a single line JavaScript comment
+
+    /* document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    */
+
+}
+
+console.log(main());
+
+module.exports = main;
