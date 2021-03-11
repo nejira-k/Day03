432ad84e37b82f2df301a7a0dd45ffd56b15b216
nejira-k <kadric.nejira@gmail.com>
2021-03-11 01:30:52 +0100

    Added new files

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -10,6 +10,8 @@ function main(numOne, numTwo) {
 
 }
 
-console.log(main(15, 8), (5, 28), (150, 425));
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
 
 module.exports = main;
